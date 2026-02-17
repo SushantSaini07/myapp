0163f8ab94afd1e1aa753761f27751544f3c080d
Sushant Saini (Nokia) <sushant.saini@nokia.com>
2026-02-17 20:24:07 +0530

    corrected podman in script

--- script.groovy
+++ script.groovy
@@ -4,12 +4,12 @@ def testing() {
 
 def buildImage() {
     echo "building the docker image... through script"
-    sh 'podman build -t myapp:1.0.1 .'
+    sh 'podman-remote build -t myapp:1.0.1 .'
 }
 
 def deployApp() {
     echo 'deploying the application... through script'
-    sh 'podman run -d -p 8701:8701 --name myapp myapp:1.0.1'
+    sh 'podman-remote run -d -p 8701:8701 --name myapp myapp:1.0.1'
 }
 
 return this
