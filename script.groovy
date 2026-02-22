60864da1ca36c74fb777aa37bb8975fa774a9df2
Sushant Saini (Nokia) <sushant.saini@nokia.com>
2026-02-22 10:47:14 +0530

    making sure branch name is used in container creation

--- script.groovy
+++ script.groovy
@@ -4,9 +4,10 @@ def buildImage() {
     sh 'podman-remote build  --build-arg HTTP_PROXY=$HTTP_PROXY --build-arg HTTPS_PROXY=$HTTPS_PROXY --build-arg HTTP_PROXY=$NO_PROXY -t myapp:1.0.1 .'
 }
 
-def deployApp() {
+def deployApp(branchname) {
     echo 'deploying the application... through script'
-    sh 'podman-remote run -d -p 8701:8701 --name myapp myapp:1.0.1'
+    def safeBranch = branch_name.replaceAll('/', '-') 
+    sh "podman-remote run -d -p 8701:8701 --name myapp-${safeBranch} myapp:1.0.1"
 }
 
 return this
