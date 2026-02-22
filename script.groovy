d4a98cb495023a4d4b352932243cc5006f6220cc
Sushant Saini (Nokia) <sushant.saini@nokia.com>
2026-02-22 11:11:45 +0530

    adding container removal

--- script.groovy
+++ script.groovy
@@ -11,6 +11,9 @@ def deployApp(branch_name) {
     echo 'deploying the application... through script'
     def safeBranch = branch_name.replaceAll('/', '-')
     sh "podman-remote run -d -p 8701:8701 --name myapp-${safeBranch} myapp:1.0.1"
+    echo 'removing container'
+    sh "podman-remote rm -rf myapp-${safeBranch}"
+    echo "container removed"
 }
 
 return this
